6c1bc5e4aebbb1985d6ddd24c59408a98fe396f6
Ahid-Naif <ahedqaood19@gmail.com>
2018-12-10 23:14:53 +0800

    create emotionConfiguration code to have main parameters of the facial expression recognition code

---
+++ configuration/emotionConfiguration.py
@@ -0,0 +1,9 @@
+from imutils import paths
+
+datasetPath = "dataset/fer2013/fer2013.csv" # path to csv file
+numClasses = 6
+# define the path to the output training, validation, and testing
+train_hdf5 = "dataset/hdf5/train.hdf5"
+vald_hdf5 = "dataset/hdf5/val.hdf5"
+test_hdf5 = "dataset/hdf5/test.hdf5"
+batchSize = 32
